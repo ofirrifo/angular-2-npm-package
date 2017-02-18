d54f048f2f0b49e9439cb6f1f5581d6f0a43f65b
ofirrifo <ofirrifo@gmail.com>
2017-02-18 16:44:38 +0200

    update webpack.config to work with typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,31 @@
+var webpack = require('webpack');
+var path = require('path');
+
 module.exports = {
-    entry: './app.js',
+    entry: './src/main.ts',
+
     output: {
-        filename: 'bundle.js'
-    }
+        publicPath: '',
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].js'
+    },
+
+    module:{
+        rules:[
+            {
+                test: /\.ts$/,
+                use:{
+                    loader: 'awesome-typescript-loader'
+                }
+            }
+        ],
+    },
+
+
+
+    resolve: {
+        extensions: [ '.ts', '.js' ],
+        modules: [ path.resolve(__dirname, 'node_modules') ]
+    },
+
 };
