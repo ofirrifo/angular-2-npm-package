85908c80177c0a379ba6429a22512a049013e5c4
ofirrifo <ofirrifo@gmail.com>
2017-02-21 09:01:31 +0200

    add types/node

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
+import { MyModule } from "../../npm-modules/index";
+
 
 @NgModule({
-    imports:      [ BrowserModule ],
-    declarations: [ AppComponent ],
-    bootstrap:    [ AppComponent ]
+    imports: [BrowserModule, MyModule],
+    declarations: [AppComponent],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
